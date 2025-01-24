2c51adce422e884cd83b0c1631745d4bf66c08e9
Skipitipopiti <skipitipopiti@gmail.com>
2025-01-24 15:04:54 +0100

    aggiunta pot

--- PrimeProveDaisyLeo/prova1_osc/prova1_osc.ino
+++ PrimeProveDaisyLeo/prova1_osc/prova1_osc.ino
@@ -15,18 +15,20 @@ size_t num_channels;   // size-t è un tipo di intero unsigned che ha la grandez
 
 static Oscillator osc; // Oscillator è la classe degli oscillatori
 
+float freq;
 
 
 void MyCallback(float **in, float **out, size_t size) {
-  float sine_signal;
+  osc.SetFreq(freq);
   for (size_t i=0; i < size; i++){
-    sine_signal = osc.Process();
-    out[0][i] = sine_signal;
-    out[1][i] = sine_signal;
+    float signal = osc.Process();
+    out[0][i] = signal;
+    out[1][i] = signal;
   }
 }
 
 void setup() {
+  //Serial.begin(9600);
   float sample_rate;
   // Initialize for Daisy pod at 48kHz
   hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K); // inizializza l'hardware hw come l'oggetto Daisy Seed con SampleRate specificato
@@ -35,11 +37,17 @@ void setup() {
   osc.Init(sample_rate); // con riferimento al file Oscillator.h, a partire dal sample rate costruisce variabili utili alla costruzione del segnale e setta dei valori di default per frequenza,amp,pw,phase etc
 
   osc.SetWaveform(osc.WAVE_SIN); // ref Oscillator.h-->funzione che prende come input dei numeri associati alle forme d'onda interni alla classe Oscillator (), in base a quelli e il valore di phase, calcola il vlore numerico dell'uscita.     Ad ogni ciclo phase += phase_inc dove phase_inc = f/Samplerate(sr)
-  osc.SetFreq(1000); // imposta la frequenza in Hz
+  osc.SetFreq(20); // imposta la frequenza in Hz
   osc.SetAmp(0.5); // imposta l'ampiezza dove amp = Vpp/2 (da verificare)
   
 
   DAISY.begin(MyCallback);
 }
 
-void loop() { }
+void loop() {
+  analogReadResolution(16);
+  freq = 20 + ((float) analogRead(A1)/65535)*19980;
+  delay(1);
+  //Serial.println(freq);
+  
+ }
