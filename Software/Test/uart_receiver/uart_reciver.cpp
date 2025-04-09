b78e6439c185cb1ba3fd24e953ba7449c3a232a4
vittoriofrangipani <vittorio.frangipani@gmail.com>
2025-04-09 09:47:58 +0200

    add

--- Software/Test/uart_receiver/uart_reciver.cpp
+++ Software/Test/uart_receiver/uart_reciver.cpp
@@ -10,27 +10,30 @@ UartHandler::Config config;
 DaisySeed hw;
 pentaPianta penta[3]; // Array of pentaPianta objects
 Effect effect[4]; // Array of Effect objects
+
+uint8_t start_bit = 'S';
 void getData()
 {
-	char start_bit = 'S';
 	uint8_t data[3];
 	while (start_bit != 'B') // Wait for the start bit
 	{
 		uart.BlockingReceive((uint8_t*)&start_bit, sizeof(start_bit), 100);
 	}
+	hw.PrintLine("Il test è iniziato \n");
 	for (size_t i = 0; i < 3; i++)
 	{
+		 // Update the pentaPianta object i
 		uart.BlockingReceive(data, 3, 100); // Receive 3 bytes of data with a timeout of 1 second
 		if (data[0] == 0)
 		{
 			penta[i].zeroCount(); // Set the first parameter of pentaPianta object i
-			penta[i].setRawValue(((uint16_t)data[1]<<8)+(uint16_t)data[2]);
 		}
 		else
 		{
 			penta[i].incCount();					  // Increment the count of the number of times the sensor has been read
-			penta[i].setRawValue(((uint16_t)data[1]<<8)+(uint16_t)data[2]); // Set the second parameter of pentaPianta object i
 		}
+		penta[i].setRawValue(((uint16_t)data[2]<<8)+(uint16_t)data[1]);
+
 	}
 		for (size_t i = 0; i < 4; i++)
 	{
@@ -38,13 +41,13 @@ void getData()
 		if (data[0] == 0)
 		{
 			effect[i].setGate(false); // Set the first parameter of pentaPianta object i
-			effect[i].setRawValue(((uint16_t)data[1]<<8)+(uint16_t)data[2]);
 		}
 		else
 		{
 			effect[i].setGate(true);					  // Increment the count of the number of times the sensor has been read
-			effect[i].setRawValue(((uint16_t)data[1]<<8)+(uint16_t)data[2]); // Set the second parameter of pentaPianta object i
 		}
+		penta[i].setRawValue(((uint16_t)data[2]<<8)+(uint16_t)data[1]);
+
 	}
 }
 
@@ -64,10 +67,18 @@ int main(void)
 	uint8_t start_bit = 'S';
 	while (start_bit != 'I') // Wait for the start bit
 	{
-		uart.BlockingReceive(&start_bit, sizeof(start_bit), 100);
+		uart.BlockingReceive(&start_bit, 1, 1000);
+		hw.PrintLine("waiting I \n");
+		hw.PrintLine("start bit %d \n", start_bit);
 	}
+
 	while (1)
 	{
 		getData(); // Call the function to get data from the UART
+		for (size_t i = 0; i < 3; i++)
+		{
+			hw.PrintLine("Il valore della pianta è: %d\n",penta[i].getRawValue()); // Update the pentaPianta object i
+			hw.PrintLine("Il valore del gate è %d \n",penta[i].getGate());  // Print the pentaPianta object i
+		}
 	}
 }
